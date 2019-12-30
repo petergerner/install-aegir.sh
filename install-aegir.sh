4d0863f5dd33f44edf33157836e150a496febabc
petergerner <pgerner@gmail.com>
2019-12-30 11:59:52 +0100

    Update install-aegir.sh

--- install-aegir.sh
+++ install-aegir.sh
@@ -6,120 +6,113 @@
 #
 
 # ***********************************
-# set versions Aegir & Drush versions
-DRUSH_VERSION="8.3.2"
-# see: https://github.com/drush-ops/drush/releases
+# set versions for Aegir & Drush
+DRUSH_VERSION="8.3.2"  # 2019-11-26
+# https://github.com/drush-ops/drush/releases
+AEGIR_VERSION="3.182"  # 2019-10-08
+# https://docs.aegirproject.org
 #
-AEGIR_VERSION="3.182"
-#
-# TBD
-MYSQL_ROOT_PASSWORD="strongpassword"
-MYSQL_AEGIR_DB_USER="aegir_root"
-MYSQL_AEGIR_DB_PASSWORD="strongpassword"
+# TODO
+#   choose webserver: now only Nginx, later: Apache or Nginx
+#   choose PHP version: now the default PHP on the server, later: set explicit version
+#   choose how to run PHP: now PHP-FPM, later PHP-FMP or mod_php
+# TODO
+MYSQL_ROOT_PASSWORD     = "strongpassword"
+MYSQL_AEGIR_DB_USER     = "aegir_root"
+MYSQL_AEGIR_DB_PASSWORD = "strongpassword"
 #
 # ***********************************
 #
 #
 # 1. install software requirements for Aegir
-
-# apt update -qq &> /dev/null
 apt update -y
 
-# aegir dependencies as per control file, and the mc
+# all aegir dependencies as per control file, and the mc
 # https://git.drupalcode.org/project/provision/blob/7.x-3.x/debian/control
-# sudo, adduser, ucf, curl, git-core, unzip, lsb-base, rsync, nfs-client
-
-# apt -y install adduser    &> /dev/null
-apt -y install curl       &> /dev/null
-apt -y install git-core   &> /dev/null
-# apt -y install lsb-base   &> /dev/null
-apt -y install nfs-client &> /dev/null
-apt -y install rsync      &> /dev/null
-# apt -y install sudo       &> /dev/null
-# apt -y install ucf        &> /dev/null
-apt -y install unzip      &> /dev/null
+# deps: sudo, adduser, ucf, curl, git-core, unzip, lsb-base, rsync, nfs-client
+# Debian 10:
+# apt -y install adduser
+# apt -y install lsb-base
+# apt -y install sudo
+# apt -y install ucf
+apt -y install curl
+apt -y install git-core
+apt -y install nfs-client
+apt -y install rsync
+apt -y install unzip
 # just for fun
-apt -y install mc         &> /dev/null
-
+apt -y install mc
 # SSL, TBC     !!!!
-apt -y install ssl-cert &> /dev/null
+apt -y install ssl-cert
 
-# UFW TBD  install + config !!!!!!!
-# apt -y install ufw &> /dev/null
-
+# TODO: UFW install + config !!!!!!!
+# apt -y install ufw
 
 # Database
-apt -y install mariadb-client &> /dev/null
-apt -y install mariadb-server &> /dev/null
+apt -y install mariadb-client
+apt -y install mariadb-server
 
 # MTA
-# add no config option
-apt -y install postfix &> /dev/null
+# TODO: Postfix config !!!!!!!
+apt -y install postfix
 
 # PHP packages
-#   1. worker
-# apt -y install php7.2-fpm &> /dev/null
-apt -y install php-fpm &> /dev/null
+#   1. how to run PHP
+# install the default PHP package on distro
+apt -y install php-fpm
+# explicit PHP version:   apt -y install php7.2-fpm
+# alternative to php-fpm: apt -y install libapache2-mod-php7.2
 
-#   alternative to php-fpm:
-# apt -y install libapache2-mod-php7.2 &> /dev/null
-
-#   2. for Drupal & Aegir
-# apt -y install php7.2-mysql php7.2-gd php7.2-xml &> /dev/null
+#   2. PHP libraries for Drupal & Aegir
 apt -y install php-mysql php-gd php-xml
+# apt -y install php7.2-mysql php7.2-gd php7.2-xml
 
-#   3. for CiviCRM
-# apt -y install php7.2-mbstring php7.2-curl php7.2-zip &> /dev/null
+#   3. PHP libraries for CiviCRM
 apt -y install php-mbstring php-curl php-zip
+# apt -y install php7.2-mbstring php7.2-curl php7.2-zip
 
-# webserver
-apt -y install nginx &> /dev/null
-# apt -y install apache2 &> /dev/null
+# Webserver
+apt -y install nginx
+# apt -y install apache2
 
 
 # 2. LAMP configurations
 #
-# PHP: set higher memory limits
+# TODO: set higher PHP limits for Nginx, Apache: upload file, memory_limit, ...
 # sed -i 's/memory_limit = -1/memory_limit = 192M/' /etc/php5/cli/php.ini
 # sed -i 's/memory_limit = 128M/memory_limit = 192M/' /etc/php5/apache2/php.ini
 #
 # Apache
 # a2enmod rewrite
-# ln -s /var/aegir/config/apache.conf /etc/apache2/conf.d/aegir.conf
-
-# on Ubuntu 14.04+
 # ln -s /var/aegir/config/apache.conf /etc/apache2/conf-available/aegir.conf
 # a2enconf aegir
-
 #
 # nginx
-# make sure your nginx installation is up and running
+# TODO: make sure your nginx installation is up and running
 ln -s /var/aegir/config/nginx.conf /etc/nginx/conf.d/aegir.conf
 # Do not reload/restart Nginx after running these commands, it will fail.
 
 # MySQL
 # remove the anonymous, passwordless login
 mysql_secure_installation
-
 # create user aegir_root
-# If you are running your Aegir databases on a remote DB server, you will want to create this aegir_root user.
-mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" --execute="GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_AEGIR_DB_USER'@'%' IDENTIFIED BY '$MYSQL_AEGIR_DB_PASSWORD' WITH GRANT OPTION;"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" \
+--execute="GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_AEGIR_DB_USER'@'%' \
+IDENTIFIED BY '$MYSQL_AEGIR_DB_PASSWORD' WITH GRANT OPTION;"
 
 # enable all IP addresses to bind, not just localhost
-# sed -i 's/bind-address/#bind-address/' /etc/mysql/my.cnf
+# TODO: locate .cnf file: sed -i 's/bind-address/#bind-address/' /etc/mysql/my.cnf
 service mysql restart
 
 # add Aegir user
-adduser --system --group --home /var/aegir aegir
+adduser --system --group --home /var/aegir aegir --shell /usr/bin/bash
 adduser aegir www-data
+# TDC: usermod -aG www-data aegir
+# TBC: usermod -aG users aegir
 
-# usermod -aG www-data aegir
-# usermod -aG users aegir
-
-#
 # sudo rights for the Aegir user to restart Apache
 echo 'aegir ALL=NOPASSWD: /etc/init.d/nginx    # for Nginx'  | tee /tmp/aegir
-echo 'aegir ALL=NOPASSWD: /usr/sbin/apache2ctl # for Apache' | tee -a /tmp/aegir
+# echo 'aegir ALL=NOPASSWD: /usr/sbin/apache2ctl # for Apache' | tee /tmp/aegir
 chmod 0440 /tmp/aegir
 mv /tmp/aegir /etc/sudoers.d/aegir
 
@@ -127,20 +120,19 @@ mv /tmp/aegir /etc/sudoers.d/aegir
 compvers=ba13e3fc70f1c66250d1ea7ea4911d593aa1dba5
 wget https://raw.githubusercontent.com/composer/getcomposer.org/$compvers/web/installer -O - -q | php -- --quiet
 sudo mv composer.phar /usr/bin/composer
-# composer --version
+su - aegir -c "composer --version"
 
 # Drush 8
 # Pick the latest stable Dursh 8 version
 # https://github.com/drush-ops/drush/releases
-DRUSH_VERSION=8.3.2  # 2019-11-26
 wget https://github.com/drush-ops/drush/releases/download/$DRUSH_VERSION/drush.phar -O - -q > /usr/local/bin/drush
 chmod +x /usr/local/bin/drush
-drush init  --add-path=/var/aegir --bg -y
+su - aegir -c "drush init  --add-path=/var/aegir --bg -y"
 
 # variables for Aegir
 echo "ÆGIR | -------------------------"
 echo 'ÆGIR | Hello! '
-echo 'ÆGIR | When the database is ready, we will install Aegir with the following options:'
+echo 'ÆGIR | We will install Aegir with the following options:'
 HOSTNAME=`hostname --fqdn`
 # AEGIR_MAKEFILE="http://cgit.drupalcode.org/provision/plain/aegir-release.make?h=$AEGIR_VERSION"
 # AEGIR_MAKEFILE="https://raw.githubusercontent.com/omega8cc/provision/4.x/aegir-release.make"
